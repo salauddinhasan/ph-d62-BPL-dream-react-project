0b419dbd265184563ad785db23417edc8beb732b
salauddin <salauddin@hasan.com>
2026-03-25 19:39:55 +0600

    feat: add toggle between available and selected players

--- src/Players.jsx
+++ src/Players.jsx
@@ -1,14 +1,28 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import AvailablePlayers from "./AvailablePlayers";
 
 const Players = ({ playersPromise }) => {
   // console.log(playersPromise);
   const players = use(playersPromise);
   console.log(players);
+
+
+  const [selected, setSelected] = useState('available')
+  console.log(selected);
+
+
   return(
     <div>
-        <h2>players {players.length}</h2>
-
+        <div className="flex items-center justify-between py-5 ">
+          <h1 className="text-3xl font-bold">Available Players</h1>
+          <div>
+            <button 
+            onClick={() => setSelected('available')}
+            className={`btn ${selected === 'available' ? 'bg-amber-200' : ''}`}>Available</button>
+            <button onClick={() => setSelected('selected')}
+            className={`btn ${selected === 'selected' ? 'bg-amber-200' : ''}`}>Selected</button>
+          </div>
+        </div>
         <AvailablePlayers players ={players}/>
     </div>
   )
