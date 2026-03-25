e940e92602fa1f8a77cfca9155477910afc685ab
salauddin <salauddin@hasan.com>
2026-03-25 10:09:01 +0600

    Create Navbar component with logo and coin button

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+   
+
+  return (
+    <div className='max-w-[1400px] mx-auto p-3'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
